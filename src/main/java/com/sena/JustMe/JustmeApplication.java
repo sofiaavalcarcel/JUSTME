c28cc3647a84ea14f9eb4feedceea80f6b8d22ba
sofiavalk <esofiavalcarcel73@gmail.com>
2025-08-28 17:06:19 -0500

    Primercommit

---
+++ src/main/java/com/sena/JustMe/JustmeApplication.java
@@ -0,0 +1,13 @@
+package com.sena.JustMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/JustMe/JustmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.JustMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
