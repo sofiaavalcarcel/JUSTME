99c2fd535cfcab8dad4eca79d6711dabbc5dbf3b
santiago <Santiagoeslava906@gmail.com>
2025-08-29 13:24:50 -0500

    Exclude de bases de datos_ESLAVA

--- src/main/java/com/sena/JustMe/JustmeApplication.java
+++ src/main/java/com/sena/JustMe/JustmeApplication.java
@@ -2,9 +2,10 @@ package com.sena.JustMe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class JustmeApplication {
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+ 
+@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
+public class JustmeApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JustmeApplication.class, args);
