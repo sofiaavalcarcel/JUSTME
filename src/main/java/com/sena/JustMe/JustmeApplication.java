52e709e69c3baf014e0bc4cf50d135d535e70cb4
yarii <yarithmanrique@gmail.com>
2025-09-05 19:48:45 -0500

    CorrecionRepositorioServicios_yarii_05/09/2025

--- src/main/java/com/sena/JustMe/JustmeApplication.java
+++ src/main/java/com/sena/JustMe/JustmeApplication.java
@@ -2,8 +2,11 @@ package com.sena.JustMe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
- 
+@EnableJpaRepositories(basePackages = "com.sena.JustMe.repository")
+@EntityScan(basePackages = "com.sena.JustMe.model")
 @SpringBootApplication 
 public class JustmeApplication  {
 
--- src/main/java/com/sena/JustMe/repository/IServiciosRepository.java
+++ src/main/java/com/sena/JustMe/repository/IServiciosRepository.java
@@ -4,6 +4,7 @@ package com.sena.JustMe.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sena.JustMe.model.Servicios;
 
+
 public interface IServiciosRepository extends JpaRepository<Servicios, Integer> {
 }
 
